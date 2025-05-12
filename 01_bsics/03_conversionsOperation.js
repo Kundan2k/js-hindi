031a7323e8656fc7527e5d56b2c4a42f8eead7b2
Kundan2k <kundankumar.icot@gmail.com>
2025-05-12 17:49:43 +0000

    conversion of datatypes

---
+++ 01_bsics/03_conversionsOperation.js
@@ -0,0 +1,27 @@
+let score = null
+
+//console.log (typeof score);
+//console.log (typeof(score));
+
+let valueNumber = Number(score)
+//console.log(typeof valueNumber);
+//console.log(valueNumber);
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = "khushi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false 
+// "kundan" => true
+
+let someNumber =33
+
+let stringnumber = String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringNumber);
