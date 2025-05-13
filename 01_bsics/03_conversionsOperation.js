3738e7f83b7cb07905ad88e1757efc90bebc2638
Kundan2k <kundankumar.icot@gmail.com>
2025-05-13 04:07:52 +0000

    operations

--- 01_bsics/03_conversionsOperation.js
+++ 01_bsics/03_conversionsOperation.js
@@ -23,5 +23,38 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber =33
 
 let stringnumber = String(someNumber)
-console.log(stringnumber);
-console.log(typeof stringNumber);
+//console.log(stringnumber);
+//console.log(typeof stringNumber);
+
+
+//********************** Operations ***************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+
+let srt1 = "hello"
+let str2 = "kundan"
+
+//let str3 = str1 + str2 
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log( (3 + 4) *5 % 3);
+
+//console.log(+true);
+//console.log(+"");
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
